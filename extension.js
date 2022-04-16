8a722bf7911a9f5b2e48787ac70634205fddb029
Max <sonthaya.nt@hotmail.com>
2022-04-16 16:39:56 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,11 +1,11 @@
 ({
-    name: "EMG Sensors", // Category Name
+    name: "EMG-AD8211",
     description: "SG00165 EMG Sensors",
     author: "AJ.TAY",
-    category: "Sensors",
+    category: "Sensors", // Category Name
     version: "1.0.0",
     icon: "/static/icon.png", // Category icon
-    color: "#0f3058", // Category color (recommend some blocks color)
+    color: "#16b5aa", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
 	"EMG_Sensors_read",
 	"EMG_Sensors_signal",
